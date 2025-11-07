f48ec350514cdb41df3c399ee606e4222caf5021
F3IT3 <davimadruga@outlook.com.br>
2025-11-07 19:37:27 -0300

    FEAT mark and winner verify

--- main.c
+++ main.c
@@ -4,19 +4,48 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i,j;
+	int l, c, linha = 0, coluna = 0;
+	char marca = 'O';
 	bool gameover = false;
 	char lista[3][3] = {{' ', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}};
+	
 	while(!gameover){
-	for(i=0;i<3;i++){
-		for(j=0;j<3;j++){
-			printf("%c",lista[i][j]);
-			if (j < 2) printf("|");
+		
+	if (marca == 'X') marca = 'O';
+	else marca = 'X';
+	
+	//Printar tabuleiro
+	for(l=0;l<3;l++){
+		for(c=0;c<3;c++){
+			printf("%c",lista[l][c]);
+			if (c < 2) printf("|");
 		}
-		if (i < 2) printf("\n------\n");
+		if (l < 2) printf("\n------\n");
 	}
-	gameover = true;
+	printf("\nVez do jogador %c",marca);
+	printf("\nLinha: ");
+	scanf("%d",&linha);
+	printf("Coluna: ");
+	scanf("%d",&coluna);
+	
+	if (lista[linha][coluna] != ' '){
+		printf("Celula preenchida\n");
+		continue;
 	}
+	else {
+		lista[linha][coluna] = marca;
+	}
+	//verificar vencedor
+	for(l=0;l<3;l++){
+		if(lista[l][0] != ' ' && lista[l][0] == lista[l][1] && lista[l][0] == lista[l][2]) gameover = true;
+		}
+	for(c=0;c<3;c++){
+		if(lista[0][c] != ' ' && lista[0][c] == lista[1][c] && lista[0][c] == lista[2][c]) gameover = true;
+		}
+	if (lista[0][0] == lista[1][1] && lista[0][0] == lista[2][2] && lista[0][0] != ' ' || 
+	lista[0][2] == lista[1][1] && lista[0][2] == lista[2][0] && lista[0][2] != ' ') gameover = true;
+	}
+	
 	printf("\n\n");
 	return 0;
 }
